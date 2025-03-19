f47a77c284bac98910c6f881c4b2ac2258ab007a
Eskil Østbø <eskilostbo@Eskil-sin-MacBook-Air.local>
2025-03-19 20:19:32 +0100

    Time in state calculations

--- challenge-1-Wokwi/src/main.cpp
+++ challenge-1-Wokwi/src/main.cpp
@@ -15,12 +15,18 @@
 #define uS_TO_S_FACTOR 1000000
 #define TIME_TO_SLEEP 31
 
+unsigned long startIdle = micros();
+
 // Define the MAC address of the central sink node
 uint8_t broadcastAddress[] = {0x8C, 0xAA, 0xB5, 0x84, 0xFB, 0x90};
 
 // Create peer interface
 esp_now_peer_info_t peerInfo;
 
+//deep sleep = 31s, idle = ca 42009us, wifi on = ca 220906us, sensor_reading = 23528us, transmission time = ca 497us
+
+// state transitions: 
+// off/deep_sleep -> idle (booting) -> wifi_on -> sensor_reading -> transmission -> deep_sleep 
 
 int measureDistance()
 {
@@ -31,6 +37,8 @@ int measureDistance()
   digitalWrite(TRIG_PIN, LOW);
 
   long duration = pulseIn(ECHO_PIN, HIGH);
+  Serial.print("\r\nDuration in read_sensor state:\t" + String(duration) + " us");
+
   int distance = duration * 0.034 / 2;
   Serial.print("\r\nDistance (cm):\t");
   Serial.println(distance);
@@ -48,12 +56,19 @@ void OnDataRecv(const uint8_t *mac, const uint8_t *data, int data_len)
 {
   Serial.print("Received data:\t");
   Serial.println((char *)data);
+  Serial.println("");
 }
 
 void setup()
 {
   // Initialize
   Serial.begin(115200);
+  unsigned long endIdle = micros();
+  unsigned long idleTime = endIdle - startIdle;
+  Serial.println("Idle state time:\t" + String(idleTime));  // idleTime state is the time in wakeup state, before the WiFi is turned on. 
+  
+  // Turn on WiFi
+  unsigned long startWiFiOn = micros();
   WiFi.mode(WIFI_STA);
   esp_now_init();
 
@@ -71,17 +86,30 @@ void setup()
   peerInfo.encrypt = false;
   esp_now_add_peer(&peerInfo);
 
+  // WiFi is not turned off, but it transitions to read_sensor state. 
+  unsigned long endWiFiOn = micros();
+  unsigned long wiFiOnTime = endWiFiOn - startWiFiOn;
+  Serial.println("WiFiOn state time:\t" + String(wiFiOnTime));
+
   // Measure the distance
+  unsigned long startReadSensor = micros();  
   int distance = measureDistance();
+  unsigned long endReadSensor = micros();  
+  unsigned long readSensorTime = endReadSensor - startReadSensor;
+  Serial.println("read_sensor :\t" + String(readSensorTime));
 
   // Send the message
+  unsigned long startTransmission = micros();  // Timestamp before sending
   String status = distance < DISTANCE_THRESHOLD ? "Occupied" : "Free";
   Serial.println("Sending data:\t" + String(status));
   esp_now_send(broadcastAddress, (uint8_t *)status.c_str(), status.length());
+  unsigned long endTransmission = micros();  // Timestamp after sending
+  unsigned long transmissionTime = endTransmission - startTransmission;
+  Serial.println("Transmission state time:\t" + String(transmissionTime));
 
   // // Prepare to sleep
   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-  Serial.println("ESP32 sleep every " + String(TIME_TO_SLEEP) + " Seconds\n");
+  Serial.println("ESP32 sleep for " + String(TIME_TO_SLEEP) + " Seconds\n");
   Serial.flush();
   esp_deep_sleep_start();
   Serial.println("This will never be printed");
